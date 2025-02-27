99d8033c3d5f790dff2231ca4df2d697aadff76d
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2025-02-27 13:19:04 +0200

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/redis-developer/go-redis-entraid
+
+go 1.18
+
+
---
+++ version.go
@@ -0,0 +1,7 @@
+package redis-entraid
+
+// Version is the current release version.
+func Version() string {
+	return "0.0.1"
+}
+
