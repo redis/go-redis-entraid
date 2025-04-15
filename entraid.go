9d087bc29c12844471ca0a05fe04dea5684ad985
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2025-04-15 14:13:32 +0300

    export shared types

---
+++ entraid.go
@@ -0,0 +1,12 @@
+package entraid
+
+import "github.com/redis-developer/go-redis-entraid/shared"
+
+// IdentityProvider is an alias for the shared.IdentityProvider interface.
+type IdentityProvider = shared.IdentityProvider
+
+// IdentityProviderResponse is an alias for the shared.IdentityProviderResponse interface.
+type IdentityProviderResponse = shared.IdentityProviderResponse
+
+// IdentityProviderResponseParser is an alias for the shared.IdentityProviderResponseParser interface.
+type IdentityProviderResponseParser = shared.IdentityProviderResponseParser
